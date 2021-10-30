7c981ee947ecc19874e5f8d713125d1c24c63c21
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-30 12:44:04 -0300

    Created the model Value

---
+++ src/main/java/com/project/consumingrestful/model/Value.java
@@ -0,0 +1,36 @@
+package com.project.consumingrestful.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+
+    private Long id;
+    private String quote;
+
+    public Value () {};
+
+    public Long getId() {
+        return getId();
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
