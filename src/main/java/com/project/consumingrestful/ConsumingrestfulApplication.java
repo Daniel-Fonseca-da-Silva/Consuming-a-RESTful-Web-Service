0a629d1f86e7d1ce2722a94d21cb2d84a60bd039
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-30 12:45:50 -0300

    Configured the main

--- src/main/java/com/project/consumingrestful/ConsumingrestfulApplication.java
+++ src/main/java/com/project/consumingrestful/ConsumingrestfulApplication.java
@@ -1,13 +1,35 @@
 package com.project.consumingrestful;
 
+import com.project.consumingrestful.model.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumingrestfulApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ConsumingrestfulApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingrestfulApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 
 }
